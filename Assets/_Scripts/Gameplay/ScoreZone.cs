41c84916ca7652a157701abab585c99637b0dc2b
Florian Lernould <florianlernould7@gmail.com>
2023-08-05 15:49:19 +0200

    Add Scoring zone
    Zone pour déposer les objets et gagner des points

---
+++ Assets/_Scripts/Gameplay/ScoreZone.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(BoxCollider))]
+public class ScoreZone : MonoBehaviour
+{
+    #region Variables
+    public static event Action<ItemSO> OnItemScored;
+    private List<PickableItem> _scoredItems;
+    #endregion
+
+    #region Builts_In
+    private void Awake()
+    {
+        _scoredItems = new List<PickableItem>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.TryGetComponent(out PickableItem item))
+            return;
+
+        AddItem(item);
+    }
+    #endregion
+
+    #region Methods
+    /// <summary>
+    /// Add an item to the list
+    /// </summary>
+    private void AddItem(PickableItem item)
+    {
+        if (_scoredItems.Contains(item))
+            return;
+
+        Debug.Log($"New item : {item.ItemInfos.ItemName} : {item.ItemInfos.Score}");
+        _scoredItems.Add(item);
+        OnItemScored?.Invoke(item.ItemInfos);
+    }
+    #endregion
+}
---
+++ Assets/_Scripts/Managers/GameManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GameManager : SingletonClass<GameManager>
+{
+    #region Variables
+
+    #endregion
+
+    #region Buitls_In
+
+    #endregion
+
+    #region Methods
+
+    #endregion
+}
